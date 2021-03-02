0f71b788cf610f105e83972ea8279101f4c24449
Adriel Santos <adriel.fariasan@gmail.com>
2021-03-02 20:03:14 -0300

    correcao de template

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<!--<meta name="csrf-token" content="{{ csrf_token() }}">-->
 
 		<title>Entrar | Pães & Delícias</title>
 
