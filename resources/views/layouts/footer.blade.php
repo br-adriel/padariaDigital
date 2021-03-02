2f021fb37cf83fa8720d8e558831a7ac89354971
Adriel Santos <adriel.fariasan@gmail.com>
2021-03-02 11:54:16 -0300

    implementacao do logout

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+	<div class="container-fluid">
+		<nav class="float-left">
+		<ul>
+			<li>
+				<h3>&copy; Pães & Delícias</h3> 
+			</li>
+			<li>
+				<h6>Telefone.: +55 (84) 9-9999-9999</h6>
+				<h6>Email.: PaesDelicias@gmail.com</h6>
+			</li>
+			<li>
+				<h6>Rua.: aquela ali que contruiram, 999, centro</h6>
+				<h6>Na lateral da esquina que fica na ponta</h6>
+			</li>
+		</ul>
+		</nav>
+	</div>
+</footer>
