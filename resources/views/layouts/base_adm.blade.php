f26274b5bdee2bc6dd8426d4ba28ccc0b08d37aa
Adriel Santos <adriel.fariasan@gmail.com>
2021-03-02 16:16:08 -0300

    implementado criar comida e listar comidas

--- resources/views/layouts/base_adm.blade.php
+++ resources/views/layouts/base_adm.blade.php
@@ -47,7 +47,7 @@ The above copyright notice and this permission notice shall be included in all c
           </a>
         </li>
         <li class="nav-item ">
-          <a class="nav-link" href="#">
+          <a class="nav-link" href="{{ route('comidas.index') }}">
             <i class="material-icons">add_shopping_cart</i>
             <p>Produtos</p>
           </a>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComidaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::Resource('comidas', ComidaController::class)->except([
+	'create',
+]);
 
 require __DIR__.'/auth.php';
