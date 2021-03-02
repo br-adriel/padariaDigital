d6c47428b30572d3ad4bba84de2fe2ee2ff053eb
Adriel Santos <adriel.fariasan@gmail.com>
2021-03-02 11:54:16 -0300

    implementacao do logout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,87 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.base_adm')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@section('titulo')
+Página inicial
+@endsection
+
+@section('titulo_pagina')
+Página inicial
+@endsection
+
+@section('conteudo')
+<div class="content">
+		<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-sm-6 mb-4">
+			<div class="card portfolio-item" style="width: 18rem;">
+				<img class="img-fluid" src="{{ asset('assets/img/portfolio/pao.jpg') }}" alt="" height="400px" width="400px"/>
+				<div class="card-body">
+				<span class="badge badge-warning">Pães</span>
+				<h4 class="card-title">Pão Frances</h4>
+				<h5 class="card-text text-danger">R$.: 0,10</h5>
+				</div>
+			</div>
+			</div>
+
+			<div class="col-lg-4 col-sm-6 mb-4">
+			<div class="card portfolio-item" style="width: 18rem;">
+				<img class="img-fluid" src="{{ asset('assets/img/portfolio/bolo.jpg') }}" alt="" height="400px" width="400px"/>
+				<div class="card-body">
+				<span class="badge badge-info">Bolo</span>
+				<h4 class="card-title">red velvet</h4>
+				<h5 class="card-text text-danger">R$.: 50,00</h5>
+				</div>
+			</div>
+			</div>
+
+			<div class="col-lg-4 col-sm-6 mb-4">
+			<div class="card portfolio-item" style="width: 18rem;">
+				<img class="img-fluid" src="{{ asset('assets/img/portfolio/salgado.jpg') }}" alt="" height="400px" width="400px"/>
+				<div class="card-body">
+				<span class="badge badge-danger">Salgados</span>
+				<h4 class="card-title">Kibe</h4>
+				<h5 class="card-text text-danger">R$.: 2,00</h5>
+				</div>
+			</div>
+			</div>
+
+			<div class="col-lg-4 col-sm-6 mb-4">
+			<div class="card portfolio-item" style="width: 18rem;">
+				<img class="img-fluid" src="{{ asset('assets/img/portfolio/bolo1.jpg') }}" alt="" height="400px" width="400px"/>
+				<div class="card-body">
+				<span class="badge badge-info" >Bolo</span>
+				<h4 class="card-title">Bolo vulcão de nutela e leite ninho</h4>
+				<h5 class="card-text text-danger">R$.: 68,00</h5>
+				</div>
+			</div>
+			</div>
+
+			<div class="col-lg-4 col-sm-6 mb-4">
+			<div class="card portfolio-item" style="width: 18rem;">
+				<img class="img-fluid" src="{{ asset('assets/img/portfolio/salgado1.jpg') }}" alt="" height="400px" width="400px"/>                        <div class="card-body">
+				<span class="badge badge-danger">salgados</span>
+				<h4 class="card-title">Coxinha de franco com Catupiry</h4>
+				<h5 class="card-text text-danger">R$.: 3,50</h5>
+				</div>
+			</div>
+			</div>
+
+			<div class="col-lg-4 col-sm-6 mb-4">
+			<div class="card portfolio-item" style="width: 18rem;">
+				<img class="img-fluid" src="{{ asset('assets/img/portfolio/pao1.jpg') }}" alt="" height="400px" width="400px"/>
+				<div class="card-body">
+				<span class="badge badge-warning">Pães</span>
+				<h4 class="card-title">Pão de 7 grãos</h4>
+				<h5 class="card-text text-danger">R$.: 18,00</h5>
+				</div>
+			</div>
+			</div>
+
+		</div>
+		</div>
+
+
+
+		@include('layouts.footer')
+	</div>
+@endsection
